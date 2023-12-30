622bbf846f7dcd651c89b4b3eaced6b1255f6897
Paula Suárez <paulasp24@icloud.com>
2023-12-30 18:46:09 +0100

    docker funcionando para desarrollo

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,9 +4,23 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ruta de entrada a la app
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'] // Lista de dependencias a pre-bundlear
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000' // Restapi
-    }
+    },
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // Puerto por defecto de Vite
   }
 })
