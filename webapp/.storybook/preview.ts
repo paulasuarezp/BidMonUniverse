45b371ff6e00e8d58d95706ac94f1287f0cabc3b
Paula Suárez <paulasp24@icloud.com>
2024-01-15 19:07:39 +0100

    prueba de funcionamiento de storybook✅

--- webapp/.storybook/preview.ts
+++ webapp/.storybook/preview.ts
@@ -1,4 +1,12 @@
 import type { Preview } from "@storybook/react";
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { lightTheme, darkTheme } from '../src/themes';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +17,28 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-  },
+  }, 
 };
 
+export const decorators = [
+  withThemeFromJSXProvider({
+  themes: {
+    light: lightTheme,
+    dark: darkTheme,
+  },
+  defaultTheme: 'light',
+  Provider: ThemeProvider,
+  GlobalStyles: CssBaseline,
+})];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    expanded: true, // Adds the description and default columns
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
 export default preview;
