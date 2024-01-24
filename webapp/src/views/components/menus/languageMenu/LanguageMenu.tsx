13560e51d258dd72dc9d16f2df6b8fa5066e4909
Paula Suárez <paulasp24@icloud.com>
2024-01-24 23:09:56 +0100

    menus de header

---
+++ webapp/src/views/components/menus/languageMenu/LanguageMenu.tsx
@@ -0,0 +1,48 @@
+import { IconButton, MenuItem, Menu } from '@mui/material';
+
+import TranslateIcon from '@mui/icons-material/Translate';
+
+//#region PROPS
+interface LanguageMenuProps {
+    anchorElLang: null | HTMLElement;
+    handleLanguageMenu: (event: React.MouseEvent<HTMLElement>) => void;
+    handleCloseLanguageMenu: (languageKey: string) => void;
+  }
+//#endregion
+
+//#region COMPONENTE LANGUAGE MENU
+export default function LanguageMenu({ anchorElLang, handleLanguageMenu, handleCloseLanguageMenu }: LanguageMenuProps) {
+    return (
+        <>
+        <IconButton color="inherit" aria-label="change language" onClick={handleLanguageMenu}>
+          <TranslateIcon />
+        </IconButton>
+        
+        <Menu
+            id="menu-appbar-language"
+            anchorEl={anchorElLang}
+            anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+            }}
+            open={Boolean(anchorElLang)}
+            onClose={handleCloseLanguageMenu}
+        >
+            <MenuItem onClick={() => handleCloseLanguageMenu('es')}>
+            <img width="30" height="30" src="https://img.icons8.com/fluency/48/spain-circular.png" alt="spain-circular"/>
+            ES
+            </MenuItem>
+            <MenuItem onClick={() => handleCloseLanguageMenu('en')}>
+            <img width="30" height="30" src="https://img.icons8.com/color/96/000000/great-britain-circular.png" alt="great-britain-circular"/>
+            EN
+            </MenuItem>
+        </Menu>
+      </>
+    );
+  }
+//#endregion
---
+++ webapp/src/views/components/menus/userMenu/UserMenu.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Menu, MenuItem, IconButton } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Button from '../../button/Button';
+import ButtonLogin from '../../button/login/ButtonLogin';
+
+
+//#region PROPS
+interface UserMenuProps {
+    auth: boolean;
+    anchorElUser: null | HTMLElement;
+    handleUserMenu: (event: React.MouseEvent<HTMLElement>) => void;
+    handleCloseUserMenu: () => void;
+  }
+//#endregion
+  
+
+//#region COMPONENTE USER MENU
+export default function UserMenu({ auth, anchorElUser, handleUserMenu, handleCloseUserMenu }: UserMenuProps) {
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate('/login'); 
+    };
+
+    return (
+      <>
+        {auth ? (
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleUserMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        ) : (
+          //<Button label='Iniciar sesión' buttonType='header' onClick={handleLoginClick} />
+          <ButtonLogin />
+        )}
+        <Menu
+          id="menu-appbar-user"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {auth && (
+            // Opciones del menú para usuario autenticado
+            <MenuItem onClick={handleCloseUserMenu}>Mi perfil</MenuItem>
+          )}
+        </Menu>
+      </>
+    );
+  };
+  
+//#endregion
