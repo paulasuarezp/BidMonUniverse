3f84a95f7e90aab2ce6a8db0bbe429967a7a5a65
Paula Suárez <paulasp24@icloud.com>
2024-04-06 16:20:32 +0200

    responsive header

--- webapp/src/views/pages/BasePage.tsx
+++ webapp/src/views/pages/BasePage.tsx
@@ -1,12 +1,39 @@
 import Header from '../components/header/Header';
 import Container from '../components/container/Container';
+import { Box } from '@mui/material';
+import { useState, useEffect } from 'react';
+
+// Hook para obtener el tamaño de la ventana
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+  
+    useEffect(() => {
+      const updateSize = () => {
+        setSize([window.innerWidth, window.innerHeight]);
+      };
+      window.addEventListener('resize', updateSize);
+      updateSize(); 
+  
+      return () => window.removeEventListener('resize', updateSize); 
+    }, []);
+  
+    return size;
+  }
 
 //#region COMPONENT BasePage
 export const BasePage = (props: { toggleTheme:any, children: any }) => {
+    const [width] = useWindowSize();
+    const isLaptop = width<=1536 && width>1024;
+    
+    
     return (
         <Container disableGutters >
             <Header toggleTheme={props.toggleTheme}/>
-            {props.children}
+            <Box sx={{mt: isLaptop? '3.5em' : 0}}>
+            <Container>
+                {props.children}
+            </Container>
+            </Box>
         </Container>
     );
 }
