ceb5919fa7b9138f48156ffbde4a2e8e0a4225bb
Paula Suárez <paulasp24@icloud.com>
2024-04-08 20:18:21 +0200

    prueba carta

--- webapp/src/views/pages/Home.tsx
+++ webapp/src/views/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ButtonPrincipal from '../components/button/principal/ButtonPrincipal';
+import PokemonCard from '../components/card/PokemonCard';
 
 //#region COMPONENTE LOGIN
 export default function Login() {
   return (
-    <ButtonPrincipal label="Inicia sesión o regístrate" />
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+    <PokemonCard name="Prueba" category='Common' backgroundImage='/selva.avif' pokemonImage='/2.svg'/>
+    </div>
   );
 };
 //#endregion
