2bd9486544e0547fd9b15864beef799ecc902955
paulasuarezp <paulasp24@icloud.com>
2024-06-09 12:59:44 +0200

    comentarios JSDoc

--- restapi/src/controllers/auctionController.ts
+++ restapi/src/controllers/auctionController.ts
@@ -10,11 +10,17 @@ import { AuctionStatus } from '../models/utils/enums';
 
 
 /**
- * Función para obtener todas las subastas.
- * @param req
- * @param res
- * @returns subastas
- * @throws 500 - Si se produce un error al obtener las subastas
+ * Recupera todas las subastas disponibles en la base de datos.
+ * Esta función busca y devuelve todas las subastas registradas, independientemente de su estado.
+ * Es ideal para obtener una vista general de todas las subastas en el sistema.
+ * 
+ * @param {Request} req - El objeto de solicitud HTTP, no se utiliza en la función pero es necesario por la estructura de los controladores.
+ * @param {Response} res - El objeto de respuesta HTTP utilizado para enviar las subastas recuperadas o un mensaje de error.
+ * 
+ * @returns {void} - No retorna un valor directamente, pero envía una respuesta HTTP con las subastas en formato JSON.
+ * Si ocurre un error durante la recuperación, se devuelve un estado 500 con un mensaje de error.
+ * 
+ * @throws {Error} 500 - Si se produce un error al intentar recuperar las subastas desde la base de datos.
  */
 const getAuctions = async (req: Request, res: Response) => {
     try {
@@ -27,12 +33,19 @@ const getAuctions = async (req: Request, res: Response) => {
 }
 
 /**
- * Función para obtener una subasta dado su id.
- * @param req
- * @param res
- * @returns subasta
- * @throws 404 - Si no se encuentra la subasta
- * @throws 500 - Si se produce un error al obtener la subasta
+ * Recupera una subasta específica por su ID.
+ * Esta función busca una subasta en la base de datos utilizando el ID proporcionado en los parámetros de la solicitud.
+ * Devuelve la subasta si se encuentra, o un error si la subasta no existe o no puede ser recuperada por algún problema técnico.
+ * 
+ * @param {Request} req - El objeto de solicitud HTTP, que debe incluir el ID de la subasta en `req.params.id`.
+ * @param {Response} res - El objeto de respuesta HTTP utilizado para enviar la subasta recuperada o un mensaje de error.
+ * 
+ * @returns {void} - No retorna un valor directamente, pero envía una respuesta HTTP. Si la subasta se encuentra,
+ * devuelve un estado 200 y la subasta en formato JSON. Si la subasta no se encuentra, devuelve un estado 404 con un mensaje de error.
+ * Si ocurre un error al recuperar la subasta, se devuelve un estado 500 con un mensaje de error.
+ * 
+ * @throws {Error} 404 - Si no se encuentra la subasta con el ID proporcionado.
+ * @throws {Error} 500 - Si se produce un error durante el proceso de recuperación de la subasta.
  */
 const getAuction = async (req: Request, res: Response) => {
     try {
@@ -49,11 +62,16 @@ const getAuction = async (req: Request, res: Response) => {
 
 
 /**
- * Función para obtener todas las subastas activas.
- * @param req
- * @param res
- * @returns subastas activas
- * @throws 500 - Si se produce un error al obtener las subastas activas
+ * Recupera todas las subastas activas en el sistema.
+ * Esta función busca en la base de datos todas las subastas que se encuentran con el estado 'open' y las devuelve.
+ * Es útil para mostrar a los usuarios una lista de subastas en las que pueden participar actualmente.
+ * 
+ * @param {Request} req - El objeto de solicitud HTTP, no se usa directamente pero es necesario para la estructura de la función.
+ * @param {Response} res - El objeto de respuesta HTTP utilizado para enviar las subastas recuperadas al cliente.
+ * 
+ * @returns {Promise<void>} - No retorna un valor directamente, pero envía una respuesta HTTP con las subastas activas en formato JSON.
+ * 
+ * @throws {Error} 500 - Se lanza un error si hay un problema al intentar recuperar las subastas activas de la base de datos.
  */
 const getActiveAuctions = async (req: Request, res: Response) => {
     try {
@@ -66,12 +84,19 @@ const getActiveAuctions = async (req: Request, res: Response) => {
 }
 
 /**
- * Función para obtener todas las subastas activas de un usuario.
- * @param req request con el username del usuario
- * @param res
- * @returns subastas activas del usuario
- * @throws 500 - Si se produce un error al obtener la subasta activa del usuario
+ * Recupera todas las subastas activas asociadas a un usuario específico.
+ * La función busca en la base de datos todas las subastas que están abiertas ('Open') y que corresponden
+ * al nombre de usuario proporcionado en los parámetros del request. Si se encuentran, las devuelve en formato JSON.
+ * 
+ * @param {Request} req - El objeto de solicitud HTTP, debe incluir el username del usuario en req.params.
+ * @param {Response} res - El objeto de respuesta HTTP donde se envían las subastas activas o un mensaje de error.
+ * 
+ * @returns {Promise<void>} No retorna un valor directamente, pero envía una respuesta HTTP con las subastas activas del usuario
+ * o un mensaje de error en caso de fallo.
+ * 
+ * @throws {Error} 500 - Si se produce un error durante la recuperación de las subastas activas del usuario.
  */
+
 const getActiveAuctionByUser = async (req: Request, res: Response) => {
     try {
         const auctions = await Auction.find({ status: AuctionStatus.Open, sellerUsername: req.params.username });
@@ -86,23 +111,22 @@ const getActiveAuctionByUser = async (req: Request, res: Response) => {
 
 
 /**
- * Función para poner una carta en subasta.
- * 1. Verificar que el usuario exista.
- * 2. Verificar que la carta exista.
- * 3. Verificar que la carta no esté en subasta.
- * 4. Actualizar la UserCard para indicar que está en subasta.
- * 5. Registrar la transacción de carta en subasta.
- * 6. Añadir la referencia de la transacción a la carta.
- * 7. Crear la subasta, con la fecha de finalización calculada en base a la duración. Si no se especifica, se establece en 48 horas. 
- * Como máximo, la duración de la subasta es de 3 días (72 horas) y como mínimo, de 2 horas.
- * 8. Registrar la subasta.
- * 9. Realizar la transacción.
- * 10. Retornar un mensaje de éxito con la subasta y la carta en subasta.
+ * Pone una carta en subasta, realizando una serie de validaciones y creando registros asociados.
  * 
- * @param req request con el username, userCardId, saleBase y duration
- * @param res 
- * @returns {UserCard, Transaction} 200 - Carta puesta en subasta
- * @throws 500 - Si se produce un error al poner la carta en subasta
+ * Proceso:
+ * 1. Verifica la existencia del usuario y que sea el propietario de la carta.
+ * 2. Confirma que la carta existe y no está ya en subasta.
+ * 3. Actualiza el estado de la carta a 'En subasta'.
+ * 4. Registra la transacción de la carta en subasta.
+ * 5. Añade la referencia de la transacción a la carta.
+ * 6. Crea la subasta con una duración definida (predeterminada a 48 horas si no se especifica).
+ * 7. Registra la subasta en la base de datos.
+ * 8. Realiza la transacción y devuelve un mensaje de éxito.
+ * 
+ * @param {Request} req - Request con el username, userCardId, saleBase y duration.
+ * @param {Response} res - Response donde se envía el resultado de la operación.
+ * @returns {Promise<void>} Devuelve mediante la respuesta HTTP la confirmación de la subasta y el objeto de la carta actualizado.
+ * @throws {Error} 500 - Si se produce un error al poner la carta en subasta, describiendo el motivo específico del fallo.
  */
 const putUserCardUpForAuction = async (req: Request, res: Response) => {
     const session = await mongoose.startSession();
@@ -209,21 +233,26 @@ const putUserCardUpForAuction = async (req: Request, res: Response) => {
 }
 
 /**
- * La carta se retira de la subasta porque no se ha vendido en el plazo establecido o porque el vendedor/administrador decide retirarla.
- * 1. Verificar que el usuario exista.
- * 2. Verificar que la carta exista.
- * 3. Verificar que la carta esté en subasta.
- * 4. Verificar que la subasta exista.
- * 5. Verificar que la subasta esté abierta.
- * 6. Actualizar la UserCard para indicar que no está en subasta.
- * 7. Actualizar la subasta.
- * 8. Registrar la transacción de carta retirada de la subasta.
- * 9. Añadir la referencia de la transacción a la carta.
- * 10. Realizar la transacción.
- * 11. Retornar un mensaje de éxito con la carta retirada de la subasta y la transacción.
+ * Retira una carta de la subasta, ya sea porque el plazo ha expirado sin ventas o por decisión del vendedor o administrador.
+ * El proceso incluye verificar la existencia y estado tanto del usuario como de la carta y la subasta correspondiente,
+ * actualizar el estado de la carta a 'No en subasta', cancelar la subasta, y registrar una transacción que refleja la retirada de la carta.
+ * Finalmente, actualiza la referencia de la transacción en la carta y confirma todos los cambios mediante una transacción de base de datos.
  * 
- * @param req request con el username, userCardId, cardId y auctionId
- * @param res {UserCard, Transaction} 200 - Carta retirada de la subasta
+ * Pasos detallados:
+ * 1. Verificar que el usuario exista.
+ * 2. Verificar que la carta exista y esté en subasta.
+ * 3. Confirmar que la subasta asociada esté abierta.
+ * 4. Actualizar el estado de la carta a 'No en subasta'.
+ * 5. Actualizar y cerrar la subasta.
+ * 6. Registrar la transacción de retirada de la subasta.
+ * 7. Añadir la referencia de la transacción a la carta.
+ * 8. Confirmar la transacción en la base de datos.
+ * 9. Devolver un mensaje de éxito.
+ * 
+ * @param {Request} req - Request con el username, userCardId y auctionId necesarios para identificar el usuario, la carta y la subasta.
+ * @param {Response} res - Response donde se envía el resultado de la operación, incluyendo un mensaje de éxito o un error.
+ * @returns {Promise<void>} - La función devuelve mediante la respuesta HTTP el resultado de la operación.
+ * @throws {Error} 500 - Se lanza un error si no se puede completar la operación, detallando el motivo específico del fallo.
  */
 const withdrawnUserCardFromAuction = async (req: Request, res: Response) => {
     const session = await mongoose.startSession();
@@ -318,9 +347,17 @@ const withdrawnUserCardFromAuction = async (req: Request, res: Response) => {
 }
 
 /**
- * Función para verificar todas las subastas activas y cerrar las que ya han finalizado.
- * @returns
- * @throws 500 - Si se produce un error 
+ * Verifica todas las subastas activas y cierra aquellas que han finalizado, determinando también los ganadores.
+ * Esta función recupera todas las subastas con estado 'Open', compara la fecha de finalización con la fecha actual,
+ * y cierra las subastas que han finalizado. Además, invoca la función `checkWinnerBid` para cada subasta cerrada
+ * con el fin de procesar y determinar el ganador de la subasta.
+ * 
+ * @param {Request} req - El objeto de solicitud HTTP. No se utiliza directamente en la función.
+ * @param {Response} res - El objeto de respuesta HTTP. Se utiliza para enviar una respuesta al cliente.
+ * 
+ * @returns {Promise<void>} - No retorna ningún valor, pero envía una respuesta HTTP con detalles de las subastas cerradas.
+ * 
+ * @throws {Error} - Lanza un error con código 500 si se produce un error al verificar las subastas activas.
  */
 const checkAllActiveAuctions = async (req: Request, res: Response) => {
     const session = await mongoose.startSession();
@@ -351,10 +388,25 @@ const checkAllActiveAuctions = async (req: Request, res: Response) => {
 
 
 /**
- * Función para verificar la subasta y asignar el ganador de la subasta.
- * @param auction subasta
- * @param session
- * @returns
+ * Verifica el ganador de una subasta y procesa la transferencia de la carta al mismo.
+ * 
+ * Proceso:
+ * 1. Obtener todas las pujas para la subasta especificada con estado 'Pending'.
+ * 2. Filtrar las pujas para incluir solo aquellas con un precio mayor o igual al precio base.
+ * 3. Ordenar las pujas filtradas por precio de manera descendente.
+ * 4. Iterar sobre las pujas ordenadas para verificar que el usuario ganador tenga suficiente saldo para comprar la carta.
+ * 5. Actualizar el estado de la subasta a 'Closed'.
+ * 6. Actualizar el estado de la puja ganadora a 'Won'.
+ * 7. Transferir la carta al usuario ganador y registrar la transacción correspondiente.
+ * 8. Marcar todas las demás pujas como 'Lost'.
+ * 
+ * @param {Auction} auction - La subasta a verificar.
+ * @param {Session} session - Datos de la sesión actual, utilizados para validar y procesar la transacción.
+ * 
+ * @returns {Promise<{message: string, updatedCard: Card, saleTransaction: Transaction, purchaseTransaction: Transaction, bid: Bid}>} 
+ * - Promesa que resuelve con detalles de la operación, incluyendo mensajes y transacciones.
+ * 
+ * @throws {Error} - Si no se puede verificar el ganador, si la transferencia de la carta falla, o si la transacción no puede completarse.
  */
 const chekWinnerBid = async (auction: IAuction, session: any) => {
     try {
@@ -366,8 +418,7 @@ const chekWinnerBid = async (auction: IAuction, session: any) => {
         }
 
         
-
-        // Obtener todas las pujas para la subasta específica y que tengan estado 'Active'
+        // Obtener todas las pujas para la subasta específica y que tengan estado 'Pending'
         const allBids: IBid[] = await Bid.find({ auction: auction._id, status: BidStatus.Pending }).exec();
 
         // Filtrar las pujas para incluir solo aquellas con un price >= basePrice
@@ -391,6 +442,16 @@ const chekWinnerBid = async (auction: IAuction, session: any) => {
         }
 
         if (winnerBid) {
+
+            // Marcar las demás pujas como perdidas
+            for (let i = 0; i < sortedBids.length; i++) {
+                if (allBids[i].status === BidStatus.Pending && allBids[i]._id !== winnerBid?._id) {
+                    allBids[i].status = BidStatus.Rejected;
+                    allBids[i].endDate = new Date();
+                    await allBids[i].save({ session });
+                }
+            }
+
             // Actualizar la subasta
             auction.status = AuctionStatus.Closed;
             auction.endDate = new Date();
@@ -409,6 +470,7 @@ const chekWinnerBid = async (auction: IAuction, session: any) => {
             return await transferCard(auction, winnerBid, session);
 
         }
+
     }
     catch (error: any) {
         console.error(error);
@@ -419,16 +481,34 @@ const chekWinnerBid = async (auction: IAuction, session: any) => {
 
 
 /**
- * Esta función transfiere una carta de un usuario a otro, registrando las transacciones de venta y compra.
- *  
- * @param req
- * @param res
- * @returns
- *  
- * @throws Error
- * - Si la carta no se encuentra o ya fue vendida.
- * - Si no se puede completar la transacción.
- **/
+ * Transfiere una carta de un usuario vendedor a un usuario comprador, registrando las transacciones de venta y compra.
+ * Esta función maneja la lógica completa del proceso de transferencia, incluyendo la verificación de los balances de los usuarios,
+ * la actualización de la posesión de la carta, y el registro de las transacciones correspondientes.
+ * 
+ * Proceso:
+ * 1. Verificar que el usuario vendedor exista.
+ * 2. Verificar que el usuario comprador exista.
+ * 3. Verificar que el comprador tenga suficiente saldo para realizar la compra.
+ * 4. Transferir el dinero entre los usuarios.
+ * 5. Actualizar la UserCard para transferirla al nuevo usuario.
+ * 6. Registrar la transacción de venta.
+ * 7. Registrar la transacción de compra.
+ * 8. Añadir la referencia a la transacción de compra de la carta.
+ * 9. Retornar un mensaje de éxito con los detalles de la operación.
+ * 
+ * @param {IAuction} auction - Objeto de subasta que contiene información del vendedor, la carta y detalles de la subasta.
+ * @param {IBid} bid - Objeto de puja que incluye la identificación del comprador y el precio de la oferta.
+ * @param {any} session - Sesión de base de datos para manejar la transacción.
+ * 
+ * @returns {Promise<{message: string, updatedCard: UserCard, saleTransaction: Transaction, purchaseTransaction: Transaction, bid: IBid}>}
+ * Una promesa que resuelve con los detalles de la operación realizada incluyendo mensaje de éxito y los documentos actualizados.
+ * 
+ * @throws {Error} - Se lanzan errores en los siguientes casos:
+ *  - Si alguno de los usuarios (vendedor o comprador) no se encuentra.
+ *  - Si el comprador no tiene suficiente saldo para realizar la compra.
+ *  - Si la carta no se encuentra o ya ha sido vendida.
+ *  - Si no se puede completar la transacción por cualquier otro motivo técnico.
+ */
 const transferCard = async (auction:IAuction, bid: IBid, session:any) => {
     try {
 
@@ -509,7 +589,7 @@ const transferCard = async (auction:IAuction, bid: IBid, session:any) => {
         await purchaseTransaction.save({ session });
 
         // Añadir la referencia de la transacción a la carta
-        updatedCard.transactionHistory.push(saleTransaction._id, purchaseTransaction._id);
+        updatedCard.transactionHistory.push(purchaseTransaction._id);
         await updatedCard.save({ session });
 
         return { message: 'La carta se ha transferido al comprador.', updatedCard: updatedCard, saleTransaction: saleTransaction, purchaseTransaction: purchaseTransaction, bid: bid };
