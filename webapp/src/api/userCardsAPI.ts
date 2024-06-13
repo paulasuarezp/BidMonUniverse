da1ecb12ff3b6e6f66ef5ab34c92da90c6c3fe5a
paulasuarezp <paulasp24@icloud.com>
2024-06-13 18:15:11 +0200

    cartas de usuario recuperadas en front

---
+++ webapp/src/api/userCardsAPI.ts
@@ -0,0 +1,60 @@
+const apiEndPointBase = 'http://localhost:5001/usercards'; // Base URL for the User API endpoints
+
+
+export const getUserCards = async (username: string) => {
+    const token = localStorage.getItem('userToken');
+    if (!token) {
+        return { error: 'No se ha encontrado un token de usuario válido, por favor, vuelva a iniciar sesión' };
+    }
+
+    const url = `${apiEndPointBase}/${username.toLowerCase()}`
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                return { error: 'No se han podido obtener las cartas de su colección, inténtelo de nuevo más tarde' };
+            }
+
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Ha ocurrido un error al obtener las cartas de su colección:', error.message);
+            return { error: 'No se han podido obtener las cartas de su colección, inténtelo de nuevo más tarde' };
+        });
+};
+
+
+export const getCardsOfUser = async (username: string) => {
+    const token = localStorage.getItem('userToken');
+    if (!token) {
+        return { error: 'No se ha encontrado un token de usuario válido, por favor, vuelva a iniciar sesión' };
+    }
+
+    const url = `${apiEndPointBase}/${username.toLowerCase()}/cards`
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                return { error: 'No se han podido obtener las cartas de su colección, inténtelo de nuevo más tarde' };
+            }
+
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Ha ocurrido un error al obtener las cartas de su colección:', error.message);
+            return { error: 'No se han podido obtener las cartas de su colección, inténtelo de nuevo más tarde' };
+        });
+};
+
