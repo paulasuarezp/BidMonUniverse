eebb30616f51df7fb27b531a42163c1e4ffd001b
paulasuarezp <paulasp24@icloud.com>
2024-06-18 16:50:41 +0200

    peueños ajustes de estilos

--- webapp/src/views/components/modals/AddAuctionForm.tsx
+++ webapp/src/views/components/modals/AddAuctionForm.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import {
-    Button, TextField, Dialog, DialogContent, DialogActions, Typography, Grow, GrowProps, Snackbar, Backdrop, CircularProgress, Alert, useTheme, Box, IconButton
+    Button, TextField, Dialog, DialogContent, DialogActions, Typography, Grow, GrowProps, CircularProgress, useTheme, Box, Fade
 } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import WarningIcon from '@mui/icons-material/Warning';
-import CloseIcon from '@mui/icons-material/Close';
+import { makeStyles } from '@mui/material/styles';
 
 const Transition = React.forwardRef(function Transition(
     props: GrowProps & { children: React.ReactElement<any, any> },
@@ -26,10 +26,8 @@ function AddAuctionForm({ open, handleClose, cardId }: AuctionModalProps) {
     const [durationError, setDurationError] = useState(false);
     const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
-    const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');
-
+    const [success, setSuccess] = useState(false);
+    const [closing, setClosing] = useState(false); // Estado adicional para el cierre suave
     const theme = useTheme();
 
     const handleCreateAuction = () => {
@@ -57,20 +55,28 @@ function AddAuctionForm({ open, handleClose, cardId }: AuctionModalProps) {
     };
 
     const handleConfirmAuction = () => {
-        setConfirmDialogOpen(false);
         setLoading(true);
 
         // Simulación de una operación asincrónica
-        setTimeout(() => {
+        new Promise<void>((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        }).then(() => {
             setLoading(false);
-            setSnackbarMessage('Tu subasta ha sido creada.');
-            setSnackbarSeverity('success');
-            setSnackbarOpen(true);
-        }, 2000);
-    };
+            setSuccess(true);
 
-    const handleSnackbarClose = () => {
-        setSnackbarOpen(false);
+            setTimeout(() => {
+                setSuccess(false);
+                setClosing(true); // Inicia el cierre suave
+
+                setTimeout(() => {
+                    setClosing(false); // Resetea el estado de cierre suave
+                    setConfirmDialogOpen(false);
+                    handleClose();
+                }, 500); // Tiempo para la transición de cierre suave
+            }, 2000); // Mostrar el mensaje de éxito durante 2 segundos antes de cerrar el diálogo
+        });
     };
 
     return (
@@ -100,6 +106,7 @@ function AddAuctionForm({ open, handleClose, cardId }: AuctionModalProps) {
                         type="text"
                         fullWidth
                         variant="outlined"
+                        sx={{ backgroundColor: '#f0f0f0' }}
                         value={cardId}
                         InputProps={{
                             readOnly: true,
@@ -155,36 +162,48 @@ function AddAuctionForm({ open, handleClose, cardId }: AuctionModalProps) {
                     }
                 }}
             >
-                <DialogContent>
-                    <WarningIcon style={{ fontSize: 40, color: theme.palette.warning.main }} />
-                    <Typography variant="h6" gutterBottom>
-                        Confirmar Subasta
-                    </Typography>
-                    <Typography>ID de la Carta: {cardId}</Typography>
-                    <Typography>Precio Base: {basePrice}</Typography>
-                    <Typography>Duración: {duration} horas</Typography>
-                </DialogContent>
-                <DialogActions style={{ justifyContent: 'center' }}>
-                    <Button onClick={() => setConfirmDialogOpen(false)} variant="contained" color='error'>
-                        Cancelar
-                    </Button>
-                    <Button onClick={handleConfirmAuction} variant="contained" color="success">
-                        Confirmar
-                    </Button>
-                </DialogActions>
+                <Fade in={!loading && !success && !closing} timeout={{ enter: 500, exit: 500 }}>
+                    <DialogContent>
+                        <Box>
+                            <WarningIcon style={{ fontSize: 50, color: theme.palette.warning.main }} />
+                            <Typography variant="h4" gutterBottom>
+                                Confirmar Subasta
+                            </Typography>
+                            <Typography variant="body2">ID de la Carta: {cardId}</Typography>
+                            <Typography variant="body2">Precio Base: {basePrice}</Typography>
+                            <Typography variant="body2">Duración: {duration} horas</Typography>
+                        </Box>
+                    </DialogContent>
+                </Fade>
+
+                <Fade in={loading} timeout={{ enter: 500, exit: 500 }}>
+                    <DialogContent>
+                        <CircularProgress />
+                    </DialogContent>
+                </Fade>
+
+                <Fade in={success} timeout={{ enter: 500, exit: 500 }}>
+                    <DialogContent>
+                        <Box display="flex" flexDirection="column" alignItems="center">
+                            <CheckCircleIcon style={{ fontSize: 50, color: theme.palette.success.main }} />
+                            <Typography variant="h6" gutterBottom>
+                                ¡Subasta Creada!
+                            </Typography>
+                        </Box>
+                    </DialogContent>
+                </Fade>
+
+                {!loading && !success && (
+                    <DialogActions style={{ justifyContent: 'center' }}>
+                        <Button onClick={() => setConfirmDialogOpen(false)} variant="contained" color='error'>
+                            Cancelar
+                        </Button>
+                        <Button onClick={handleConfirmAuction} variant="contained" color="success">
+                            Confirmar
+                        </Button>
+                    </DialogActions>
+                )}
             </Dialog>
-
-            {/* Estado de Carga */}
-            <Backdrop style={{ zIndex: 1300 }} open={loading}>
-                <CircularProgress color="inherit" />
-            </Backdrop>
-
-            {/* Notificación de Éxito/Error */}
-            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
-                <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
-                    {snackbarMessage}
-                </Alert>
-            </Snackbar>
         </div>
     );
 }
