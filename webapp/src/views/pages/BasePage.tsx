8759e115754782a8fa24f0a692a0d67065a014b6
paulasuarezp <paulasp24@icloud.com>
2024-06-18 13:00:32 +0200

    pequeños ajustes

--- webapp/src/views/pages/BasePage.tsx
+++ webapp/src/views/pages/BasePage.tsx
@@ -3,39 +3,20 @@ import Container from '../components/container/Container';
 import { Box } from '@mui/material';
 import { useState, useEffect } from 'react';
 
-// Hook para obtener el tamaño de la ventana
-function useWindowSize() {
-    const [size, setSize] = useState([0, 0]);
-  
-    useEffect(() => {
-      const updateSize = () => {
-        setSize([window.innerWidth, window.innerHeight]);
-      };
-      window.addEventListener('resize', updateSize);
-      updateSize(); 
-  
-      return () => window.removeEventListener('resize', updateSize); 
-    }, []);
-  
-    return size;
-  }
 
 //#region COMPONENT BasePage
-export const BasePage = (props: { toggleTheme:any, children: any }) => {
-    const [width] = useWindowSize();
-    const isLaptop = width<=1536 && width>1024;
-    
-    
-    return (
-        <Container disableGutters >
-            <Header toggleTheme={props.toggleTheme}/>
-            <Box sx={{mt: isLaptop? '3.5em' : 0}}>
-            <Container>
-                {props.children}
-            </Container>
-            </Box>
+export const BasePage = (props: { toggleTheme: any, children: any }) => {
+
+  return (
+    <Container disableGutters style={{ height: '100vh' }}>
+      <Header toggleTheme={props.toggleTheme} />
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        <Container>
+          {props.children}
         </Container>
-    );
+      </Box>
+    </Container>
+  );
 }
 //#endregion
 export default BasePage;
--- webapp/src/views/pages/Login.tsx
+++ webapp/src/views/pages/Login.tsx
@@ -17,53 +17,49 @@ export default function Login() {
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState(''); // Estado para almacenar el mensaje de error
 
-
-  
-  
-
   const handleLogin = async () => {
     loginAPI(username, password)
       .then(data => {
-          if (data.error) {
-              console.error('Error en el inicio de sesión desde Login.tsx -> ', data.error);
-              setErrorMessage(data.error); // Guardar y mostrar el mensaje de error
-          } else {
-              setErrorMessage(''); // Limpiar el mensaje de error en caso de éxito
-              console.log('Usuario almacenado en el estado global desde Login.tsx -> ', data);
-              dispatch(setUser(data.user));
-              if(data.token) {
-                console.log('Conectando socket desde Login.tsx -> ', data.token)
-                dispatch(setSocketConnected(true));
-                connectSocket(data.token, data.user.username);
-              }
-              // Redirigir a la página de inicio
-              navigate('/logueado');
+        if (data.error) {
+          console.error('Error en el inicio de sesión desde Login.tsx -> ', data.error);
+          setErrorMessage(data.error); // Guardar y mostrar el mensaje de error
+        } else {
+          setErrorMessage(''); // Limpiar el mensaje de error en caso de éxito
+          console.log('Usuario almacenado en el estado global desde Login.tsx -> ', data);
+          dispatch(setUser(data.user));
+          if (data.token) {
+            console.log('Conectando socket desde Login.tsx -> ', data.token)
+            dispatch(setSocketConnected(true));
+            connectSocket(data.token, data.user.username);
           }
+          // Redirigir a la página de inicio
+          navigate('/logueado');
+        }
       })
       .catch(error => {
-          console.error('Error en el inicio de sesión desde Login.tsx -> ', error);
-          setErrorMessage(error.message); // Guardar y mostrar el mensaje de error
+        console.error('Error en el inicio de sesión desde Login.tsx -> ', error);
+        setErrorMessage(error.message); // Guardar y mostrar el mensaje de error
       });
   };
 
-
   return (
-    <Paper 
-      title="Iniciar sesión" 
+    <Paper
+      title="Iniciar sesión"
       imageSrc="logo-sf.png"
       elevation={3}
-      sx={{ 
-        maxWidth: 400, 
-        mx: 'auto', 
-        pt: 2, 
-        mt: { xs: '5.5em', sm: 'auto' } 
+      sx={{
+        maxWidth: 400,
+        margin: 'auto',
+        mx: 'auto',
+        pt: 2,
+        mt: { xs: '5.5em', sm: 'auto' }
       }}
     >
-      <Box sx={{ pl: 2, pr: 2, pb:2}}>
+      <Box sx={{ pl: 2, pr: 2, pb: 2 }}>
         <TextField fullWidth label="Usuario" margin="normal" value={username} onChange={(e) => setUsername(e.target.value)} />
         <TextField fullWidth label="Contraseña" type="password" margin="normal" value={password} onChange={(e) => setPassword(e.target.value)} />
-        {errorMessage && <p style={{color: 'red'}}>{errorMessage}</p>}
-        <Button buttonType="primary" label='Iniciar sesión' fullWidth sx={{ mt: 2 }} onClick={handleLogin}/> 
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        <Button buttonType="primary" label='Iniciar sesión' fullWidth sx={{ mt: 2 }} onClick={handleLogin} />
       </Box>
     </Paper>
   );
