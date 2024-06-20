19730ebe82910a99b1267a8b297b0bbc6490bbb0
paulasuarezp <paulasp24@icloud.com>
2024-06-20 13:17:27 +0200

    Filtradas transacciones en el detalle de la carta

--- webapp/src/utils/utils.ts
+++ webapp/src/utils/utils.ts
@@ -1,3 +1,20 @@
+import { TransactionConcept } from "../shared/sharedTypes";
+
 export function capitalizeFirstLetter(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export function getTransactionMessage(concept: TransactionConcept) {
+    switch (concept) {
+        case TransactionConcept.BidCancelledFromAuction: return 'La puja se ha retirado debido a que la subasta ha sido cancelada.';
+        case TransactionConcept.BidWithdrawn: return 'La puja se ha retirado.';
+        case TransactionConcept.Gift: return 'Carta regalada';
+        case TransactionConcept.PurchaseByBid: return 'Carta adquirida mediante puja';
+        case TransactionConcept.PurchaseByCardPack: return 'Carta adquirida mediante sobre';
+        case TransactionConcept.ForSaleOnAuction: return 'Carta en subasta';
+        case TransactionConcept.NewBid: return 'Nueva puja';
+        case TransactionConcept.SoldOnAuction: return 'Carta vendida en subasta';
+        case TransactionConcept.WithdrawnFromAuction: return 'Carta retirada de la subasta';
+        default: return 'Transacción';
+    }
 }
