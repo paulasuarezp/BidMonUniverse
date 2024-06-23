f1bcbf190b9d6ba9697845c34459898ab577fee9
paulasuarezp <paulasp24@icloud.com>
2024-06-23 10:44:33 +0200

    base de tienda

--- restapi/src/routes/cardPackRoutes.ts
+++ restapi/src/routes/cardPackRoutes.ts
@@ -1,30 +1,30 @@
 const { param, validationResult } = require('express-validator');
 import express, { Request, Response, Router } from 'express';
-import  auth  from '../middlewares/authMiddleware';
+import auth from '../middlewares/authMiddleware';
 
 const cardPackRouter: Router = express.Router();
 
 cardPackRouter.use(auth);
 
 import {
-    getCardPacks,
-    getCardPack
+    getCardPack,
+    getCardPacks
 } from '../controllers/cardPackController';
 
 /**
  * Ruta para obtener todos los sobres de cartas
- * @route GET /cardPacks
+ * @route GET /
  */
 cardPackRouter.get('/', getCardPacks);
 
 /**
  * Ruta para obtener un sobre de cartas por su ID
- * @route GET /cardPacks/:cardPackId
+ * @route GET /:cardPackId
  * @param cardPackId id del sobre de cartas
  */
 cardPackRouter.get('/:cardPackId', [
     param('cardPackId').notEmpty().withMessage('Card Pack ID is required'),
-    param('cardPackId').isString().isLength({ min: 2, max: 10}).withMessage('Card Pack ID must be a valid string with a length between 2 and 10 characters'),
+    param('cardPackId').isString().isLength({ min: 2, max: 10 }).withMessage('Card Pack ID must be a valid string with a length between 2 and 10 characters'),
     (req: Request, res: Response, next: any) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -19,6 +19,7 @@ import Login from './views/pages/Login';
 import Logueado from './views/pages/Logueado';
 import MyCollection from './views/pages/MyCollection';
 import NotFoundPage from './views/pages/NotFoundPage';
+import Shop from './views/pages/Shop';
 import Signup from './views/pages/Signup';
 import UserTransactions from './views/pages/UserTransactions';
 
@@ -47,6 +48,7 @@ function App() {
                   {/* Rutas protegidas */}
                   <Route path="/logued" element={<RouteRedirector initRoute={<Logueado />} redirectPath="/login" accessLevel={AccessLevel.Standard} />} />
                   <Route path="/album" element={<RouteRedirector initRoute={<MyCollection />} redirectPath="/login" accessLevel={AccessLevel.Standard} />} />
+                  <Route path="/shop" element={<RouteRedirector initRoute={<Shop />} redirectPath="/login" accessLevel={AccessLevel.Standard} />} />
                   <Route path="/transactions" element={<RouteRedirector initRoute={<UserTransactions />} redirectPath="/login" accessLevel={AccessLevel.Standard} />} />
                   <Route path="/auctions" element={<RouteRedirector initRoute={<ActiveAuctions />} redirectPath="/login" accessLevel={AccessLevel.Standard} />} />
                   <Route path="/auctions/:id" element={<RouteRedirector initRoute={<AuctionCardDetail />} redirectPath="/login" accessLevel={AccessLevel.Standard} />} />
