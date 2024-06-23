fab800f4db37cb78416a2dc250999892510f1119
paulasuarezp <paulasp24@icloud.com>
2024-06-23 13:39:02 +0200

    consigo Transaction COMPLETED: 50J64674PP681141S. See console for all available details

--- webapp/src/views/pages/RechargeBalance.tsx
+++ webapp/src/views/pages/RechargeBalance.tsx
@@ -3,7 +3,6 @@ import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { handlePay } from '../../api/paypalAPI';
 import { RootState } from '../../redux/store';
 import Container from '../components/container/Container';
 import Paper from '../components/paper/Paper';
@@ -12,29 +11,26 @@ export default function RechargeBalance() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-
     const sessionUser = useSelector((state: RootState) => state.user);
-
     const [balance, setBalance] = useState(1); // 1 EUR = 10 zens
     const [errorMessage, setErrorMessage] = useState('');
     const [total, setTotal] = useState(1);
+    const [message, setMessage] = useState("");
     const username = sessionUser.username.toLowerCase();
-
 
     const handleTotalChange = (value: string) => {
         if (value === '' || parseInt(value) < 1) {
             setErrorMessage('Por favor, introduce una cantidad válida para recargar. Mínimo: 1 zen');
         } else {
             setErrorMessage('');
-            const total = parseInt(value);
-            setBalance(total * 10);
-            setTotal(total);
+            const totalValue = parseInt(value);
+            setBalance(totalValue * 10);
+            setTotal(totalValue);
         }
     };
 
-    const handleApprove = async (orderID: string) => {
-        await handlePay(orderID, total, username, balance);
-    };
+    const apiEndPointBase = 'http://localhost:5001/paypal'; // Base URL for the PayPal API endpoints
+
 
     return (
         <Container>
@@ -61,31 +57,72 @@ export default function RechargeBalance() {
                         value={total}
                         onChange={e => handleTotalChange(e.target.value)}
                         error={errorMessage !== ''}
-                        helperText={errorMessage ? 'Por favor, introduce una cantidad válida para recarar. Mínimo: 1 zen' : 'Valor predeterminado: 1 zen.'}
+                        helperText={errorMessage ? 'Por favor, introduce una cantidad válida para recargar. Mínimo: 1 zen' : 'Valor predeterminado: 1 zen.'}
                     />
-                    <PayPalScriptProvider options={{ clientId: "AXE7-9Er_F_oY9-RdBsCvSZQ1OBfeTbgUVzzTj1tZk9RivJRYoI5U-xNYqUgqwY0z3_M4NvC6hJ5Cg8_", currency: "EUR" }}>
+                    <PayPalScriptProvider options={{ "clientId": "AXE7-9Er_F_oY9-RdBsCvSZQ1OBfeTbgUVzzTj1tZk9RivJRYoI5U-xNYqUgqwY0z3_M4NvC6hJ5Cg8_", currency: "EUR" }}>
                         <PayPalButtons
-                            createOrder={(data, actions) => {
-                                return actions.order.create({
-                                    intent: "CAPTURE", // Añadir propiedad intent
-                                    purchase_units: [{
-                                        amount: {
-                                            currency_code: 'EUR',
-                                            value: total.toString(), // Valor dinámico del saldo
-                                        },
-                                    }],
-                                });
+                            style={{
+                                shape: "rect",
+                                layout: "vertical",
+                                color: "gold",
+                                label: "pay",
                             }}
-                            onApprove={(data, actions) => {
-                                return actions.order.capture().then(details => {
-                                    handleApprove(details.id);
-                                });
+                            createOrder={async () => {
+                                try {
+                                    const response = await fetch(`${apiEndPointBase}/orders`, {
+                                        method: "POST",
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify({
+                                            username: username,
+                                            balance: balance,
+                                            total: total,
+                                        }),
+                                    });
+
+                                    const orderData = await response.json();
+
+                                    if (orderData.id) {
+                                        return orderData.id;
+                                    } else {
+                                        const errorDetail = orderData?.details?.[0];
+                                        const errorMessage = errorDetail
+                                            ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+                                            : JSON.stringify(orderData);
+
+                                        throw new Error(errorMessage);
+                                    }
+                                } catch (error) {
+                                    console.error(error);
+                                    setMessage(`No se pudo iniciar PayPal Checkout...${error}`);
+                                }
+                            }}
+                            onApprove={async (data, actions) => {
+                                try {
+                                    const orderData = await actions.order.capture();
+                                    console.log('Capture result', orderData);
+
+                                    if (!orderData.purchase_units || !orderData.purchase_units[0].payments || !orderData.purchase_units[0].payments.captures) {
+                                        console.error('Response structure unexpected:', orderData);
+                                        throw new Error('Response structure unexpected. Check the response format.');
+                                    }
+
+                                    const transaction = orderData.purchase_units[0].payments.captures[0];
+                                    setMessage(`Transaction ${transaction.status}: ${transaction.id}. See console for all available details`);
+                                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                                } catch (error) {
+                                    console.error('Error in onApprove:', error);
+                                    setMessage(`Lo siento, su transacción no pudo ser procesada...${error.message}`);
+                                }
                             }}
                             onError={(err) => {
-                                console.error(err);
+                                console.error('Error al crear la orden:', err);
+                                setMessage(`Error al crear la orden: ${err}`);
                             }}
                         />
                     </PayPalScriptProvider>
+                    {message && <p>{message}</p>}
                 </Box>
             </Paper>
         </Container>
