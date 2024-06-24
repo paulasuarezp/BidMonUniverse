206e623afff83a9f15a92a14622bbb659f378f17
paulasuarezp <paulasp24@icloud.com>
2024-06-24 18:17:42 +0200

    mejora de estilo

--- webapp/src/views/pages/BasePage.tsx
+++ webapp/src/views/pages/BasePage.tsx
@@ -1,5 +1,6 @@
-import { Alert, Box } from '@mui/material';
-import { useEffect } from 'react';
+import { Box } from '@mui/material';
+import { Toast } from 'primereact/toast';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeNotification } from '../../redux/slices/notificationSlice';
 import { RootState } from '../../redux/store';
@@ -7,13 +8,11 @@ import { NotificationType } from '../../shared/sharedTypes';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 
-
 //#region COMPONENT BasePage
 export const BasePage = (props: { toggleTheme: any, children: any }) => {
-
+  const toast = useRef(null);
   const dispatch = useDispatch();
   const notifications = useSelector((state: RootState) => state.notificationState.notifications);
-  const hasNotification = useSelector((state: RootState) => state.notificationState.hasNotification);
 
   const handleClose = (id: string) => {
     dispatch(removeNotification(id));
@@ -28,41 +27,35 @@ export const BasePage = (props: { toggleTheme: any, children: any }) => {
       default:
         return 'info';
     }
-  }
+  };
 
   useEffect(() => {
-    console.log('Notificaciones:', notifications);
-    /*
-    const timer = setTimeout(() => {
-      if (notifications.length > 0) {
-        handleClose(notifications[0].id);
+    notifications.forEach(notification => {
+      console.log('Notification en base page:', notification);
+      if (toast.current) {
+        toast.current.show({
+          severity: getSeverity(notification.type),
+          summary: NotificationType[notification.type],
+          detail: notification.message,
+          life: 5000
+        });
+        handleClose(notification.id);
       }
-    }, 5000); // 5 segundos
-
-    return () => clearTimeout(timer);*/
-  }, [notifications, hasNotification]);
-
+    });
+  }, [notifications]);
 
   return (
     <Box display="flex" flexDirection="column" minHeight="100vh">
+      <Toast ref={toast} position="bottom-left" />
       <Header toggleTheme={props.toggleTheme} />
       <Box component="main" flexGrow={1} mt={2} mb={2}>
-        {notifications.map((notification) => (
-          <Alert
-            key={notification.id}
-            onClose={() => handleClose(notification.id)}
-            severity={getSeverity(notification.type)}
-            sx={{ mt: 10 }}
-          >
-            {notification.message}
-          </Alert>
-        ))}
-        {/* Aquí va el contenido principal de la página */}
         {props.children}
       </Box>
       <Footer />
     </Box>
   );
 }
+
 //#endregion
+
 export default BasePage;
