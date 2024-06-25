8ce1e9a5f14a75c96f0e63504ad88bda0e0213f0
paulasuarezp <paulasp24@icloud.com>
2024-06-25 13:24:36 +0200

    paginacion

--- webapp/src/views/pages/Inbox.tsx
+++ webapp/src/views/pages/Inbox.tsx
@@ -1,5 +1,5 @@
 import MailIcon from '@mui/icons-material/Mail';
-import { Divider, List, ListItem, ListItemIcon, ListItemText, Paper } from '@mui/material';
+import { Divider, List, ListItem, ListItemIcon, ListItemText, Pagination, Paper } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getUserNotifications } from '../../api/notificationsAPI';
@@ -10,6 +10,8 @@ import BasePageWithNav from './BasePageWithNav';
 export default function Inbox() {
     const sessionUser = useSelector((state: RootState) => state.user);
     const [notifications, setNotifications] = useState<Notification[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [pageSize] = useState<number>(10);
 
     useEffect(() => {
         const getNotifications = async () => {
@@ -24,11 +26,20 @@ export default function Inbox() {
         getNotifications();
     }, [sessionUser.username]);
 
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    };
+
+    // Calcular el índice de las notificaciones a mostrar
+    const startIndex = (page - 1) * pageSize;
+    const selectedNotifications = notifications.slice(startIndex, startIndex + pageSize);
+
     return (
         <BasePageWithNav title="Bandeja de entrada" showBackButton={false} handleBack={() => { }}>
+
             <Paper>
                 <List>
-                    {notifications.map((notification) => (
+                    {selectedNotifications.map((notification) => (
                         <React.Fragment key={notification._id}>
                             <ListItem button>
                                 <ListItemIcon>
@@ -44,6 +55,13 @@ export default function Inbox() {
                         </React.Fragment>
                     ))}
                 </List>
+                <Pagination
+                    count={Math.ceil(notifications.length / pageSize)}
+                    page={page}
+                    onChange={handlePageChange}
+                    color="primary"
+                    sx={{ display: 'flex', justifyContent: 'center', padding: '20px 0' }}
+                />
             </Paper>
         </BasePageWithNav>
     );
